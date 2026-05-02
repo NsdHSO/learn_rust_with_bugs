078b6daab98703cd79f928215472ddc73e05fc72
NsdHSO <nechiforelsamuel@gmail.com>
2026-05-02 12:55:43 +0300

    Fix borrowing exercise - use mutable borrow

--- archive/02-borrowing/src/main.rs
+++ archive/02-borrowing/src/main.rs
@@ -6,7 +6,7 @@ fn main() {
     // r1 is dropped before r2 is created, or restructure the logic.
     let mut s = String::from("hello");
     let r1 = &mut s;
-    let r2 = &mut s;
+    let r2 = &r1;
     println!("{r1} {r2}");
 
     // ERROR 2: mutable borrow while immutable borrows exist
@@ -15,13 +15,13 @@ fn main() {
     let mut t = String::from("world");
     let r3 = &t;
     let r4 = &t;
-    let r5 = &mut t;
+    let r5 = &r4;
     println!("{r3} {r4} {r5}");
 
     // ERROR 3: mutating through a shared (immutable) reference
     // HINT: push_str needs &mut self. Change &u to &mut u to allow mutation.
-    let u = String::from("immutable");
-    let r6 = &u;
-    r6.push_str(" nope");
+    let mut u = String::from("immutable");
+    let r6 = &mut u;
+     r6.push_str(" nope");
     println!("{r6}");
 }
